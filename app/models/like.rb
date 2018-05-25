33db65f56d03fc95a1403b9e84d97d7c5c5a5aa9
matsushitashuhei <shuhei@matsushitashuuheinoMacBook-Air.local>
2018-05-25 10:13:02 +0900

    add assosiation to models

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,6 @@
 class Like < ActiveRecord::Base
+  belongs_to :user
+  belomgs_to :prototypes
+  validates :user_id, presence: true
+  validates :prototypes_id, presence: true
 end
--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -2,7 +2,8 @@ class Prototype < ActiveRecord::Base
   belongs_to :user
   has_many :captured_images, dependent: :destroy
   has_many :comments
-
+  has_many :likes
+  
   accepts_nested_attributes_for :captured_images, reject_if: :reject_sub_images
 
   validates :title,
