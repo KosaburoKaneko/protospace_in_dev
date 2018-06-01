a1e374a277bd81de638db7f482f1734fef148a17
matsushitashuhei <shuhei@matsushitashuuheinoMacBook-Air.local>
2018-06-01 10:29:18 +0900

    Modify controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,15 +2,16 @@ class LikesController < ApplicationController
 
   def create
    @like = Like.create(user_id: current_user.id, prototype_id: params[:prototype_id])
-   @likes = Like.where(prototype_id: params[:prototype_id])
-   @prototypes = Prototype.all
+   # likes_selectedとprototypes_allを/concern/common.rbにて定義
+   likes_selected
+   prototypes_all
   end
 
   def destroy
    like = Like.find_by(user_id: current_user.id, prototype_id: params[:prototype_id])
    like.destroy
-   @likes = Like.where(prototype_id: params[:prototype_id])
-   @prototypes = Prototype.all
+   likes_selected  
+   prototypes_all
   end
 end
 
