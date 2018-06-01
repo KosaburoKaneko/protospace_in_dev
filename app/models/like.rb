6b07e1ef9d2737b60f18c4f0c045c858b5d8d184
matsushitashuhei <shuhei@matsushitashuuheinoMacBook-Air.local>
2018-06-01 09:20:16 +0900

    modify like.rb

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,8 @@
 class Like < ActiveRecord::Base
   belongs_to :user
-  belomgs_to :prototype, counter_cache: :likes_count
+  belongs_to :prototype, counter_cache: :likes_count
   validates :user_id, presence: true
-  validates :prototypes_id, presence: true
+  validates :prototype_id, presence: true
+
+  validates_uniqueness_of :prototype_id, scope: :user_id
 end
