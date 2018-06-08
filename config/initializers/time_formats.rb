e0644b5e156f65d72f9046cd0f5c80337b6b33a7
matsushitashuhei <shuhei@matsushitashuuheinoMacBook-Air.local>
2018-06-08 09:24:33 +0900

    modify time_formats.rb

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,5 +1,1 @@
-Time::DATE_FORMATS[:default] = '%Y/%m/%d %H:%M'
 Time::DATE_FORMATS[:datetime] = '%Y/%m/%d %H:%M'
-Time::DATE_FORMATS[:date] = '%Y/%m/%d'
-Time::DATE_FORMATS[:time] = '%H:%M:%S'
-Date::DATE_FORMATS[:default] = '%Y/%m/%d'
