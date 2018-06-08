39dfcb78bfe04eadfbfbbe36afb874bd2ab729a4
matsushitashuhei <shuhei@matsushitashuuheinoMacBook-Air.local>
2018-06-08 09:33:33 +0900

    Resolve conflicts

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,4 @@
 class LikesController < ApplicationController
-
   def create
    @like = Like.create(user_id: current_user.id, prototype_id: params[:prototype_id])
    # likes_selectedとprototypes_allを/concern/common.rbにて定義
